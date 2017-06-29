2e1be75977347acbf7fc6b8eb0ac87116d52b6fc
Majick Tek <kingtmw2@gmail.com>
2017-06-28 20:20:10 -0400

    Update cls.c

--- cls.c
+++ cls.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
-void vt100_clear() {
+void vt100_clear() { // Simple function to use a VT-100 "clear entire screen" escape sequence, also known as "˄[[2J".
     printf("\033[2J");
+    /*
+    * The backslash is programming escape, the 033 is octal escape, and the "[2J" is the
+    * VT-100 escape sequence itself.
+    * An escape "033[2J" needs to be escaped "\" to work!
+    */
 }
 
 int main() {
-   vt100_clear();
+   vt100_clear(); // simple usage of function.
 }
