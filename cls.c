3c78ba810ffecce67fa509944b74ec4ccb9254e9
Majick Tek <kingtmw2@gmail.com>
2017-07-24 09:58:37 -0400

    Update cls.c
    trying to fix formatting errors for UNIX/LINUX/BSD/etc systems

--- cls.c
+++ cls.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 void vt100_clear() { // Simple function to use a VT-100 "clear entire screen" escape sequence, also known as "˄[[2J".
-    printf("\033[2J");
+    printf("\033[2J\n");
     /*
     * The backslash is programming escape, the 033 is octal escape, and the "[2J" is the
     * VT-100 escape sequence itself.
     * An escape "033[2J" needs to be escaped "\" to work!
+    * The "\n" is the standard escape code for "newline"
     */
 }
 
